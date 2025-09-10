15859e14e47bbc72083ad72bcbc2f71bbeb0db88
Bernard Ograh <ograhbernard@gmail.com>
2025-09-10 08:31:33 +0000

    websocket url

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = "https://studypaddiesapi.up.railway.app";
 
 export const api = {
     // User registration
